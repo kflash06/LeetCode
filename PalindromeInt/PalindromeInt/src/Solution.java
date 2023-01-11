6bef8d62ef1c1912ba4f073eb43371f6a4bd479c
kflash06 <ks27543926@gmail.com>
2023-01-11 22:11:46 +0530

    Palindrome Number

---
+++ PalindromeInt/PalindromeInt/src/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int reverse = 0;
+        int temp = x;
+        while ( temp > 0 ) {
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+        return x == reverse;
+    }
+}
