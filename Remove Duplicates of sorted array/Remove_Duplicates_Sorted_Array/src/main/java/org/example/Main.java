821b561eebde0dd2ec4c2d5fc22fbabe54925e36
kflash06 <ks27543926@gmail.com>
2023-01-18 23:50:44 +0530

    Remove Duplicates from Sorted Array

---
+++ Remove Duplicates of sorted array/Remove_Duplicates_Sorted_Array/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(Solution.removeDuplicates(nums));
+    }
+}
---
+++ Remove Duplicates of sorted array/Remove_Duplicates_Sorted_Array/src/main/java/org/example/Solution.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Solution {
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+
+        int count = 1;
+
+        for(int i = 0; i < nums.length - 1; i++) {
+
+            if(nums[i] < nums[i + 1]) {
+                nums[count] = nums[i + 1];
+                count++;
+            }
+        }
+        return count;
+    }
+}
