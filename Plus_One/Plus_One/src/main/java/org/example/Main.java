956ec726d2a11e63db3a090d4ee8fb21d14b78b8
kflash06 <ks27543926@gmail.com>
2023-01-27 07:26:10 +0530

    Added Plus_One solution

---
+++ Plus_One/Plus_One/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] digits = {1,2,3,4,5};
+        System.out.println(Arrays.toString(Solution.plusOne(digits)));
+
+    }
+}
